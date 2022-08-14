45d3128fe30e3fbce55b4111173da662d2ce4871
Edwin Hamal <edwinhamal@gmail.com>
2022-08-14 22:59:57 +0545

    inital commit

---
+++ test.py
@@ -0,0 +1,15 @@
+# from random import randint
+# x = "0"*100
+# print(list(x))
+# x[randint(0,len(x)-1)] = x
+# found = 0
+# def find_x(x):
+#     if(x[0])=="X":
+#         print("found")
+#         return
+#     if(x[len(x)-1]) == "X":
+#         print("found")
+#         return
+    
+#     while !found:
+# print(randint(0, 1))
